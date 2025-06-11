3cc6ed179c8a3a3e4f75415cac10b61a3a78eda2
MicaKenigsberg <micakenigsberg@gmail.com>
2025-06-11 11:25:12 -0300

    Update consultar_clima.py

--- consultar_clima.py
+++ consultar_clima.py
@@ -2,3 +2,74 @@ import requests
 import csv
 from datetime import datetime
 import os
+
+
+API_KEY = "2ec2d4136b7cc15376f4e6d4429566b2"  
+HISTORIAL = "historial_global.csv"
+
+def consultar_clima(usuario_actual):   
+    ciudad = input("Ingrese el nombre de la ciudad: ").strip()
+    if not ciudad:
+        print("Debe ingresar un nombre de ciudad.")
+        return
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={API_KEY}&units=metric"
+
+    parametros = {
+        'q': ciudad,
+        'appid': API_KEY,
+        'units': 'metric',
+        'lang': 'es'
+    }
+
+    print(f"\nConsultando el clima en {ciudad}...")
+
+    try:
+        respuesta = requests.get(url,params=parametros, timeout=10)
+        respuesta.raise_for_status()  
+        datos = respuesta.json()
+
+        temperatura = datos["main"]["temp"]
+        sensacion = datos["main"]["feels_like"]
+        humedad = datos["main"]["humidity"]
+        viento = datos["wind"]["speed"] * 3.6 
+        descripcion = datos["weather"][0]["description"].capitalize()
+        fecha_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        print(f"\nClima en {ciudad}:")
+        print(f"Temperatura: {temperatura}°C (Sensación: {sensacion}°C)")
+        print(f"Humedad: {humedad}%")
+        print(f"Viento: {viento:.1f} km/h")
+        print(f"Descripción: {descripcion.capitalize()}")
+        print(f"Fecha y hora de consulta: {fecha_hora}")
+
+        guardar_historial_global(usuario_actual, ciudad, fecha_hora, temperatura, descripcion, humedad, viento)
+
+    except requests.exceptions.HTTPError as err:
+        if respuesta.status_code == 401:
+            print("Error: API Key inválida.")
+        elif respuesta.status_code == 404:
+            print(f"Ciudad '{ciudad}' no encontrada.")
+        else:
+            print(f"Error HTTP: {err}")
+        print(f"Error al consultar el clima: {err}")
+    
+    except requests.exceptions.RequestException as err:
+        print(f"Error de conexión: {err}")
+    
+    except KeyError:
+        print("Error: Formato inesperado en datos recibidos.")
+    except Exception as err:
+        print(f"Error inesperado: {err}")
+
+def guardar_historial_global(usuario, ciudad, fecha_hora, temp, clima, humedad, viento):
+    encabezado = ["Usuario", "Ciudad", "FechaHora", "Temperatura_C", "Condicion_Clima", "Humedad_Porcentaje", "Viento_kmh"]
+    nueva_fila = [usuario, ciudad, fecha_hora, temp, clima, humedad, round(viento, 1)]
+
+    archivo_nuevo = not os.path.exists(HISTORIAL)
+    with open(HISTORIAL, mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        if archivo_nuevo:
+            writer.writerow(encabezado)
+        writer.writerow(nueva_fila)
+    
+    print("\nConsulta guardada en el historial global.")
